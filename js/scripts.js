62d21ea35a96963b5ab700a671234ce746bacee6
Raul Escobedo <rjescobedo@gmail.com>
2024-12-18 13:42:35 -0600

    add puns

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,20 @@ darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     darkModeToggle.classList.toggle('dark-mode');
 });
+
+//Puns
+async function fetchPun() {
+    try {
+        const response = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
+        const data = await response.json();
+        if (data.joke) {
+            document.getElementById('pun').innerText = data.joke;
+        } else {
+            document.getElementById('pun').innerText = "Couldn't fetch a pun today, try again later!";
+        }
+    } catch (error) {
+        document.getElementById('pun').innerText = "Error fetching pun: " + error.message;
+    }
+}
+
+fetchPun();
