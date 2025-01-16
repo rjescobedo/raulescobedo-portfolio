6f55d317cf21a0aed216dbdefc7ee4d123b33cc6
Raul Escobedo <rjescobedo@gmail.com>
2025-01-15 19:35:02 -0600

    edits

--- dist/js/scripts.js
+++
@@ -1,9 +0,0 @@
-//This function closes the navbar when clicked outside or when a nav-item has been clicked
-$(document).click(function (event) {
-    let clickover = $(event.target);
-    let $navbar = $(".navbar-collapse");               
-    let _opened = $navbar.hasClass("in");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
-        $navbar.collapse('hide');
-    }
-});
