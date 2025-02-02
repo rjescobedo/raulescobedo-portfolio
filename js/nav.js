a5a96f5b10bde745daecc2957269bb026c596c4d
Raul Escobedo <rjescobedo@gmail.com>
2025-02-01 20:27:51 -0600

    updates

--- js/nav.js
+++ js/nav.js
@@ -23,6 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('is-active');
     });
 
+    document.querySelectorAll("a[href^='#']").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            let targetId = this.getAttribute("href").substring(1);
+            let targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+                window.history.replaceState(null, "", window.location.pathname + window.location.search);
+            }
+        });
+    });
 });
-
-window.history.replaceState(null, null, window.location.pathname + window.location.search);
